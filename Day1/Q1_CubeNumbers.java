c831cd8a6b9ab9a101fe0ff61a2bbf8cebf1d793
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-02-19 18:03:42 +0530

    Update Q1_CubeNumbers.java

--- Day1/Q1_CubeNumbers.java
+++ Day1/Q1_CubeNumbers.java
@@ -1,2 +1,11 @@
-
+public class CubeNumbers{
+  public static void main(String[] args){
+List<Integer> list=List.of(1,2,3,4);
+List<Integer> result=
+  list.stream()
+  .map(n->n*n*n)
+  .toList();
+System.out.println(result);
+  }
+}
 
