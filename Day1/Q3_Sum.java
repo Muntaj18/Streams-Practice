86afe8069fe40063997ae4d3de8cdc2ab60125d3
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-02-19 19:20:57 +0530

    Update Q3_Sum.java

--- Day1/Q3_Sum.java
+++ Day1/Q3_Sum.java
@@ -1,1 +1,11 @@
+class Sum{
+    public static void main(String[] args) {
+ List<String> list=List.of("Sam","john","Sara","Alex");
+List<String> result=
+list.stream()
+    .filter(n->n.startsWith("S"))
+    .toList();
+System.out.println(result);
 
+    }
+}
