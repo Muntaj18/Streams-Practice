af24c3ccc9c66dbe735c0de7265f6d6199b9e81e
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-02-19 19:24:33 +0530

    Create Q2_FilterNames.java

---
+++ Day1/Q2_FilterNames.java
@@ -0,0 +1,11 @@
+class FilterNames{
+    public static void main(String[] args) {
+ List<String> list=List.of("Sam","john","Sara","Alex");
+List<String> result=
+list.stream()
+    .filter(n->n.startsWith("S"))
+    .toList();
+System.out.println(result);
+
+    }
+}
