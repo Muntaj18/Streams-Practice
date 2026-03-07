3a4755193047f0011a474596ec920009b8249de8
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-03-07 23:21:44 +0530

    Create Q23.java

---
+++ Day4/Q23.java
@@ -0,0 +1,18 @@
+//Largest Number
+
+import java.util.*;
+import java.util.stream.*;
+class Main {
+    public static void main(String[] args) {
+        List<Integer> list= List.of(12,23,34,47,88);
+        
+       Optional<Integer> result=
+       list.stream()
+       .sorted(Comparator.reverseOrder())
+       .findFirst();
+       
+       System.out.println(result.get());
+        
+        
+    }
+}
