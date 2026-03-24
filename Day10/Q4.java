e3857e917ba9f3bd202bf464949131d3fdad82b8
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-03-24 12:35:58 +0530

    Add Q4.java to merge two lists

---
+++ Day10/Q4.java
@@ -0,0 +1,17 @@
+//Merge two lists.
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Q4 {
+    public static void main(String[] args) {
+
+        List<Integer> list1 = List.of(1, 2, 3);
+        List<Integer> list2 = List.of(4, 5, 6);
+
+        List<Integer> merged = Stream.concat(list1.stream(), list2.stream())
+                .collect(Collectors.toList());
+
+        System.out.println(merged);
+    }
+}
