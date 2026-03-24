a79dc08e8a4be8e92bd1499f636f30c0ba38ae74
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-03-24 12:23:18 +0530

    Add Java program to find the longest word

---
+++ Day9/Q4.java
@@ -0,0 +1,24 @@
+//Find longest word.
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class P7 {
+    public static void main(String[] args) {
+        List<String> sentences = List.of(
+
+                "Java is powerful",
+                "Java streams are powerful",
+                "Streams make Java powerful"
+        );
+
+        Optional<String> longestWord = sentences.stream()
+                .filter(s -> s != null && !s.isBlank())
+                .flatMap(s -> Arrays.stream(s.split("\\s+")))
+                .filter(w -> !w.isBlank())
+                .max(Comparator.comparingInt(String::length));
+
+        longestWord.ifPresent(System.out::println);
+    }
+}
