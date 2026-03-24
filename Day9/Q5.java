2d8c67b780f34d4e48c1e2e1bf088e50e7bf568c
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-03-24 12:24:01 +0530

    Add word frequency calculation in P8 class

---
+++ Day9/Q5.java
@@ -0,0 +1,27 @@
+//Find word frequency.
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class P8 {
+    public static void main(String[] args) {
+
+        List<String> sentences = List.of(
+                "Java is powerful",
+                "Java streams are powerful",
+                "Streams make Java powerful"
+        );
+        Map<String, Long> frequency = sentences.stream()
+                .filter(s -> s != null && !s.isBlank())
+                .flatMap(s -> Arrays.stream(s.split("\\s+")))
+                .map(String::toLowerCase)
+                .filter(w -> !w.isBlank())
+                .collect(Collectors.groupingBy(
+                        word -> word,
+                        Collectors.counting()
+                ));
+
+        System.out.println(frequency);
+    }
+}
