b236ab16eb82da6c228e0f918bf181ba4a42132f
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-03-24 12:24:30 +0530

    Rename class P8 to Main in Q5.java

--- Day9/Q5.java
+++ Day9/Q5.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class P8 {
+public class Main {
     public static void main(String[] args) {
 
         List<String> sentences = List.of(
