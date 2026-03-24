2317ad2c8f5224d94ce7804eb313bb588275291d
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-03-24 12:25:09 +0530

    Rename class P7 to Main in Q4.java

--- Day9/Q4.java
+++ Day9/Q4.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
-public class P7 {
+public class Main {
     public static void main(String[] args) {
         List<String> sentences = List.of(
 
