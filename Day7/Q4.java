f9b97a5555b008af56fd937047c0262474d95818
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-03-24 11:32:56 +0530

    Add Java program to sort map by keys

---
+++ Day7/Q4.java
@@ -0,0 +1,26 @@
+//sort by key
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Main {
+    public static void main(String[] args) {
+              Map<String,Integer> map=Map.of(
+                  "banana",2,
+                  "Apple",4,
+                  "grapes",3
+                  );
+                Map<String,Integer> result=map.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(
+                    Map.Entry::getKey,
+                    Map.Entry::getValue,
+                    (v1,v2)->v1,
+                    LinkedHashMap::new
+                    
+                    ));
+               System.out.println(result);
+        
+        
+    }
+}
