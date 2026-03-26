dcc98091be7655ce47f40d601ac2eabaee2671c8
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-03-26 12:04:54 +0530

    Create Q7.java

---
+++ Day12/Q7.java
@@ -0,0 +1,20 @@
+//reverse Strings
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main{
+    public static List<String> stringlength(List<String> list)
+    {
+        return list.stream()
+        .map(n->new StringBuilder(n).reverse().toString())
+        .toList();
+        
+    }
+    public static void main(String[] args)
+    {
+        List<String> list=List.of("muntaj","begum","shaik","gousiya");
+        
+        List<String> result=stringlength(list);
+        System.out.println(result);
+    }
+}
