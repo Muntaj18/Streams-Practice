d32b9c7fdab1259b577ebf6bffcd1f1b3a95d706
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-03-28 18:46:54 +0530

    Create Q10.java

---
+++ Day14/Q10.java
@@ -0,0 +1,26 @@
+//First non-repeating element
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+public class Main{
+    public static Optional<Integer> firstNonRepeating(List<Integer> list)
+    {
+        return list.stream()
+        .collect(Collectors.groupingBy(n->n,LinkedHashMap::new,Collectors.counting()))
+        .entrySet()
+        .stream()
+        .filter(n->n.getValue()==1)
+        .map(Map.Entry::getKey)
+        .findFirst();
+      
+    }
+    public static void main(String[] args)
+    {
+       
+        List<Integer> list=List.of(1,2,2,3,4,4,5,6,6,-1,-2,0,0);
+        
+        Optional<Integer> result=firstNonRepeating(list);
+        
+        System.out.println(result);
+    }
+}
