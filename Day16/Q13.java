0994635582be34a659a9640cf053d6c20fc12913
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-04-17 22:44:41 +0530

    Create Q13.java

---
+++ Day16/Q13.java
@@ -0,0 +1,24 @@
+//First Unique Character in a String
+class Solution {
+    public int firstUniqChar(String s) {
+
+       Map<Character,Integer> map=new LinkedHashMap<>();
+
+       for(char ch:s.toCharArray())
+       {
+        map.put(ch,map.getOrDefault(ch,0)+1);
+       }
+
+       for(Map.Entry<Character,Integer> entry:map.entrySet())
+       {
+    if(entry.getValue()==1)
+        return s.indexOf(entry.getKey());
+       }
+
+       return -1;
+
+
+        
+    }
+    
+}
