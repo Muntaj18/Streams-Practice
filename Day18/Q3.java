a32325d7c422bbe0052c3e5a07287bd85895a892
Shaik Muntaj Begum <144787910+Muntaj18@users.noreply.github.com>
2026-04-22 20:37:31 +0530

    Update Q3.java

--- Day18/Q3.java
+++ Day18/Q3.java
@@ -1,7 +1,7 @@
 //Find the Last Element in a List
 import java.util.*;
 import java.util.stream.*;
-class Main {
+class Q3 {
     public static int lastele(List<Integer> list)
     {
         return list.stream()
